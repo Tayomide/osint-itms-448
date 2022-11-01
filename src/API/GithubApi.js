70b96beb34efe6fee50badcab6a7a3164ed53129
Tayomide <tibrahim@hawk.iit.edu>
2022-11-01 16:50:09 -0500

    Create fetch promises for Github API

---
+++ src/API/GithubApi.js
@@ -0,0 +1,22 @@
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json"
+  }
+}
+
+fetch("https://dog.ceo/api/breeds/image/random")
+.then(response => response.json())
+.then(response => console.log(response))
+
+export const findUser = (user) => fetch(`https://api.github.com/search/users?q=${user}`, options)
+
+export const getCommits = () => fetch(`https://api.github.com/repos/Tayomide/hci-project-one/stats/participation`, options)
+
+export const getFollowers = (user) => fetch(`https://api.github.com/users/${user}/followers`, options)
+
+export const getOrgs = (user) => fetch(`https://api.github.com/users/${user}/orgs`, options)
+
+export const getRepos = (user) => fetch(`https://api.github.com/users/${user}/repos`, options)
+
+export const getUser = (user) => fetch(`https://api.github.com/users/${user}`, options)
