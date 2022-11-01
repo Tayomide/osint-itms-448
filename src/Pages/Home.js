9da36d4b9edfe78db229d97598a74b2f2d2e47c0
Tayomide <tibrahim@hawk.iit.edu>
2022-11-01 16:51:21 -0500

    Create home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+// import spotifyApi from "../API/spotifyApi"
+// import MetaAPi from "../API/MetaApi"
+// import GithubApi from "../API/GithubApi"
+
+import { useEffect } from "react";
+
+export const Home = () => {
+  useEffect(() => {
+    // twitterApi.then(response => {
+    //   const cloneResponse = response.clone()
+    //   return cloneResponse.json()
+    // })
+    // .then(response => console.log(response))
+    // .catch(err => console.error(err));
+  }, [])
+  
+
+  return (
+    <Container>Home</Container>
+  )
+}
+
+const Container = styled.div``
