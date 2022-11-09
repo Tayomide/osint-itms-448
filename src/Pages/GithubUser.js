d8bdc2b97f819389a09f6521e6d2872e1f959749
Tayomide <tibrahim@hawk.iit.edu>
2022-11-08 19:57:05 -0600

    Format data for download

--- src/Pages/GithubUser.js
+++ src/Pages/GithubUser.js
@@ -1,19 +1,31 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
+import { GithubUserHeader } from "../Components/GithubUserHeader"
+import styled from "styled-components"
+import { GithubStat } from "../Components/GithubStat"
 
 export const GithubUser = () => {
   const GithubApi = require("../API/GithubApi")
   const params = useParams()
   const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [fileData, setFileData] = useState("")
   useEffect(() => {
-    if(localStorage[params.user]){
-      setData(JSON.parse(localStorage[params.user]))
-    }else{
-      GithubApi.getUser(params.user)
-      .then(response => response.clone().json())
-      .then(response => setData(response))
-      .catch(e => console.error(e))
-    }
+    (
+      async function(){
+        setLoading(true)
+        if(localStorage[params.user]){
+          setData(JSON.parse(localStorage[params.user]))
+        }else{
+          await GithubApi.getUser(params.user)
+          .then(response => response.clone().json())
+          .then(response => setData(response))
+          .catch(e => console.error(e))
+        }
+        setLoading(false)
+      }
+    )()
+
     return () => {
       localStorage.removeItem(params.user)
     }
@@ -23,33 +35,80 @@ export const GithubUser = () => {
     localStorage[params.user] = JSON.stringify(data)
     console.log(data)
 
-    // Get Commits
-    GithubApi.getCommits()
-    .then(response => response.clone().json())
-    .then(response => console.log("Commits",response))
-    .catch(e => console.error(e))
+  }, [data, GithubApi, params.user])
 
-    // Get Followers
-    GithubApi.getFollowers(params.user)
-    .then(response => response.clone().json())
-    .then(response => console.log("Followers",response))
-    .catch(e => console.error(e))
+  useEffect(() => {
+    (async () => {
+      const dataFile = {"User_Data": data}
 
-    // Get Repos
-    GithubApi.getRepos(params.user)
-    .then(response => response.clone().json())
-    .then(response => console.log("Repositories",response))
-    .catch(e => console.error(e))
+      await GithubApi.getCommits()
+      .then(response => response.clone().json())
+      .then(response => dataFile["Commits"] = response)
+      .catch(e => console.error(e))
 
-    // Get Organizations
-    GithubApi.getOrgs(params.user)
-    .then(response => response.clone().json())
-    .then(response => console.log("Organizations",response))
-    .catch(e => console.error(e))
-    
+      // Get Followers
+      await GithubApi.getFollowers(params.user)
+      .then(response => response.clone().json())
+      .then(response => dataFile["Followers"] = response)
+      .catch(e => console.error(e))
+
+      // Get Repos
+      await GithubApi.getRepos(params.user)
+      .then(response => response.clone().json())
+      .then(response => dataFile["Repositories"] = response)
+      .catch(e => console.error(e))
+
+      // Get Organizations
+      await GithubApi.getOrgs(params.user)
+      .then(response => response.clone().json())
+      .then(response => dataFile["Organizations"] = response)
+      .catch(e => console.error(e))
+
+      setFileData(dataFile)
+    })()
   }, [data, GithubApi, params.user])
   
+  
   return (
-    <div>GithubUser</div>
+    <Container>
+      {!loading?
+      <>
+        <GithubUserHeader 
+        userName={data?.login}
+        data={fileData}
+        // img={data.avatar_url}
+        />
+        <GithubStatContainer>
+          <GithubStat
+          title="Public Repositories"
+          stat={data.public_repos}
+          />
+          <GithubStat
+          title="Followers"
+          stat={data.followers}
+          />
+          <GithubStat
+          title="Following"
+          stat={data.following}
+          />
+        </GithubStatContainer>
+      </>:
+      <p>Loading...</p>
+    }
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  *{
+    height: auto;
+  }
+`
+
+const GithubStatContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
